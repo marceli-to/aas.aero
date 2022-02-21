9251b3a7e86ef6f7c6001e94573cbb3472b1c672
Marcel Stadelmann <m@marceli.to>
2022-02-21 11:01:12 +0100

    staging build

--- resources/views/web/components/content/address.blade.php
+++ resources/views/web/components/content/address.blade.php
@@ -1,7 +1,13 @@
 <div class="item">
-  <div class="item__title">
-    <h2>{{ $title }}</h2>
-  </div>
+  @if (isset($title))
+    <div class="item__title">
+      <h2>{{ $title }}</h2>
+    </div>
+  @else
+    <div class="item__title hide-sm">
+      <h2>&nbsp;</h2>
+    </div>
+  @endif
   <div class="item__content">
     {{ $content}}
   </div>
--- resources/views/web/partials/addresses/sales.blade.php
+++ resources/views/web/partials/addresses/sales.blade.php
@@ -6,14 +6,12 @@
   </div>
   <div class="address__items">
     <x-address>
-      <x-slot name="title">&nbsp;</x-slot>
       <x-slot name="content">
         <p><strong>AAS Corporate Services </strong></p>
         <p>Erik Strandgren<br>Chief Commercial Officer<br>+41 76 433 74 88<br><x-mailto email="e.strandgren@aas.aero" /></p>
       </x-slot>
     </x-address>
     <x-address>
-      <x-slot name="title">&nbsp;</x-slot>
       <x-slot name="content">
         <p><strong>AAS Corporate Services </strong></p>
         <p>Nevena Dragosavljevic<br>Chief Communications Officer<br>+41 76 420 83 06<br><x-mailto email="n.dragosavljevic@aas.aero" /></p>
