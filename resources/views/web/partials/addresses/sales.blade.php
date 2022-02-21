715e581c33aff311935f8cea879969f54dade35e
Marcel Stadelmann <m@marceli.to>
2022-02-21 12:51:11 +0100

    staging build

--- resources/views/web/partials/addresses/sales.blade.php
+++ resources/views/web/partials/addresses/sales.blade.php
@@ -9,6 +9,13 @@
         <p>Erik Strandgren<br>Chief Commercial Officer<br>+41 76 433 74 88<br><x-mailto email="e.strandgren@aas.aero" /></p>
       </x-slot>
     </x-address>
+  </div>
+</div>
+<div class="addresses">
+  <div class="address__type">
+    <h3>Communication / Media</h3>
+  </div>
+  <div class="address__items">
     <x-address>
       <x-slot name="content">
         <p><strong>AAS Corporate Services </strong></p>
